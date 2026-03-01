d03c6a3a6ac6c4031f66b91a1cbbf7a51eb93c5b
joaopaulo2148 <jpbpimenta06@gmail.com>
2026-02-28 21:26:04 -0300

    feat: enviando arquivos do sistema

---
+++ EncaminhaArquivoPro/projetinhoArquivo/src/main/java/com/example/projetinhoArquivo/ProjetinhoArquivoApplication.java
@@ -0,0 +1,12 @@
+package com.example.projetinhoArquivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetinhoArquivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetinhoArquivoApplication.class, args);
+    }
+}
